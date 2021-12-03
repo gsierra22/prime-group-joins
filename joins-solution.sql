636bf68baa276eae1f02d7dad76df68f0d8103c3
Giancarlo Sierra <85034079+gsierra22@users.noreply.github.com>
2021-12-03 10:31:52 -0600

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,40 +1,44 @@
+--1--
 SELECT * FROM
 customers JOIN addresses ON
 customers.id=addresses.id;
-
+--2--
 SELECT * 
 FROM orders JOIN line_items ON
 orders.id=line_items.id;
 
+--3 Delta has cheetos--
 SELECT (warehouse) FROM warehouse 
 JOIN warehouse_product ON warehouse.id=warehouse_product.warehouse_id 
 JOIN products ON warehouse_product.product_id=products.id
 WHERE products.description ='cheetos';
 
+--4 Alpha, Delta, Gamma have diet pepsi --
 SELECT (warehouse) FROM warehouse 
 JOIN warehouse_product ON warehouse.id=warehouse_product.warehouse_id 
 JOIN products ON warehouse_product.product_id=products.id
 WHERE products.description ='diet pepsi';
-
+--5--
 SELECT customers.first_name, customers.last_name, COUNT(orders.id) FROM customers 
 JOIN addresses ON customers.id=addresses.customer_id 
 JOIN orders ON addresses.id=orders.address_id
 GROUP BY customers.id, customers.first_name;
-
+--6--
 SELECT COUNT(id) AS customer_count FROM customers; 
-
+--7--
 SELECT COUNT(id) FROM products; 
-
+--8 There are 92 available--
 SELECT SUM(warehouse_product.on_hand) FROM 
 warehouse_product JOIN products ON warehouse_product.product_id=products.id WHERE
 products.description='diet pepsi';
 
+--9 --
 SELECT orders.id, orders.order_date, SUM(products.unit_price *line_items.quantity)
 FROM orders
 JOIN line_items ON orders.id=line_items.order_id
 JOIN products ON products.id= line_items.product_id
 GROUP BY orders.id, orders.order_date ORDER BY orders.order_date DESC;
-
+--10--
 SELECT customers.first_name, customers.last_name, SUM(products.unit_price * line_items.quantity) AS total_price
 FROM orders
 JOIN line_items ON orders.id = line_items.order_id
@@ -42,7 +46,7 @@ JOIN products ON products.id = line_items.product_id
 JOIN addresses ON addresses.id = orders.address_id
 JOIN customers ON addresses.customer_id = customers.id
 GROUP BY customers.first_name, customers.last_name ORDER BY customers.last_name DESC;
-
+--11--
 SELECT customers.first_name, customers.last_name, SUM (unit_price*quantity) AS total_order_price
         FROM customers 
         JOIN addresses ON addresses.customer_id=customers.id
